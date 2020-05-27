4bf3fa28419cc3e700525c5eeb651b7587defbb1
Mark Leung <mark.leung89@gmail.com>
2020-05-27 23:06:44 +1000

    Commit with menu items

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,14 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
 import Info from '../components/Home/Info'
+import Menu from '../components/Home/Menu'
 
 const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
     <BackgroundSection img={data.img.childImageSharp.fluid} title="Regular Mark's" styleClass="default-background" />
     <Info />  
+    <Menu items={data.menu} />
   </Layout>
 )
 
@@ -23,6 +25,24 @@ export const query = graphql`
       }
     }
   }
+
+  menu:allContentfulCoffeeItem {
+	  edges {
+	    node {
+	      id,title,description {
+	        description
+	      }
+        price
+        category
+        image {
+          fixed(width:50,height:50) {
+						...GatsbyContentfulFixed_tracedSVG
+          }
+        }
+	    }
+	  }
+	}
+
 }
 `;
 
