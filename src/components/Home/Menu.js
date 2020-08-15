70210f7c570371965eba7ad21e76af558cd8c431
Mark Leung <mark.leung89@gmail.com>
2020-08-15 11:45:21 +1000

    Latest changes

--- src/components/Home/Menu.js
+++ src/components/Home/Menu.js
@@ -3,10 +3,15 @@ import Title from '../Globals/Title'
 import Img from 'gatsby-image'
 
 const getCategories = items => {
+
     let tempItems = items.map(items => {
         return items.node.category;
     });
-    return tempItems;
+    
+    let tempCategories = new Set(tempItems);
+    let categories = Array.from(tempCategories);
+    categories = ["all", ...categories];
+    return categories; 
 }
 
 export default class Menu extends Component {
@@ -20,10 +25,22 @@ export default class Menu extends Component {
         };
     }
 
-    render() {
+    handleItems = (category) => {
+        let tempItems = [ ...this.state.items ];
 
-        console.log(this.state.categories);
-        console.log(this.state.items);
+        if (category === "all") {
+            this.setState( () => {
+                return {coffeeItems: tempItems}
+            });
+        } else {
+            let items = tempItems.filter(({node}) => node.category === category);
+            this.setState( () => {
+                return {coffeeItems: items}
+            });
+        }
+    }
+
+    render() {
 
         if ( this.state.items.length > 0 ) {
             return (
@@ -31,6 +48,14 @@ export default class Menu extends Component {
                     <div className="container">
                         <Title title="best of our menu"></Title>
                                 { /* categories */ }
+                                <div className="row mb-5">
+                                    <div className="col-10 mx-auto text-center">
+                                        {this.state.categories.map( (category, index) => {
+                                            return (<button type="button" key={index} className="btn btn-yellow text-capitalize m-3" onClick={ () => {this.handleItems(category)}}>{category}</button>)
+
+                                        } )}
+                                    </div>
+                                </div>
                                 { /* items */ }                        
                             <div className="row">
                                 {this.state.coffeeItems.map(({node}) => {
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,8 @@ import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
 import Info from '../components/Home/Info'
 import Menu from '../components/Home/Menu'
+import Products from '../components/Home/Products'
+import Contact from '../components/Home/Contact'
 
 const IndexPage = ({data}) => (
   <Layout>
@@ -13,6 +15,8 @@ const IndexPage = ({data}) => (
     <BackgroundSection img={data.img.childImageSharp.fluid} title="Regular Mark's" styleClass="default-background" />
     <Info />  
     <Menu items={data.menu} />
+    <Products />
+    <Contact />
   </Layout>
 )
 
