ab1149ee8d058e461c41b33eb947a6f04d9b6361
Mark Leung <mark.leung89@gmail.com>
2020-08-20 20:25:21 +1000

    data-item-url

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -14,7 +14,7 @@ export default function Product({product}) {
                         data-item-name={product.title}
                         data-item-price={product.price}
                         data-item-image={product.image.fluid.src}
-                        data-item-url="https://mcleung.netlify.app/"
+                        data-item-url="mcleung.netlify.app/"
                     
                     
                     >
